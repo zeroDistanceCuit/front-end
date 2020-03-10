894f5ca2a20a48a6b70c1d9e367439839f70fad5
merlynr <lcq1013962426@gmail.com>
2020-03-10 10:53:21 +0800

    对商品进行管理

--- src/views/merchandise-management/merchandise-management.js
+++ src/views/merchandise-management/merchandise-management.js
@@ -1,10 +1,72 @@
 export const merchandiseManagement = {
-    data(){
-        return{
-
+    data() {
+        return {
+            options: [{
+                value: '选项1',
+                label: '黄金糕'
+            }, {
+                value: '选项2',
+                label: '双皮奶'
+            }, {
+                value: '选项3',
+                label: '蚵仔煎'
+            }, {
+                value: '选项4',
+                label: '龙须面'
+            }, {
+                value: '选项5',
+                label: '北京烤鸭'
+            }],
+            value: '',
+            input: '',
+            nums: 0,
+            dialogVisible: false,
+            table1: [{
+                id: 111,
+                name: '王小虎',
+                type: "200333"
+            }, {
+                name: '王小虎',
+                type: "200333"
+            }, {
+                name: '王小虎',
+                type: "200333"
+            }, {
+                name: '王小虎',
+                type: "200333"
+            }, {
+                name: '王小虎',
+                type: "200333"
+            }, {
+                name: '王小虎',
+                type: "200333"
+            }, {
+                name: '王小虎',
+                type: "200333"
+            }, {
+                name: '王小虎',
+                type: "200333"
+            }, {
+                name: '王小虎',
+                type: "200333"
+            }],
+            shopId: 0
         }
     },
-    methods:{
-
+    methods: {
+        add(row) {
+            this.dialogVisible = true
+            this.shopId = row.id
+        },
+        addAction(id) {
+            console.log(id)
+        },
+        delete(row) {
+            this.shopId = row.id
+            this.dialogVisible = true
+        },
+        deleteAction(id) {
+            console.log(id)
+        }
     }
 }
--- src/views/merchandise-management/merchandise-management.vue
+++ src/views/merchandise-management/merchandise-management.vue
@@ -1,16 +1,91 @@
 <template>
   <div>
-    
+    <el-card
+      shadow="never"
+      style="position:fixed;padding-bottom:10px;margin-top:-2%;z-index:auto;width:35%;height:60%"
+    >
+      <el-select
+        size="mini"
+        style="float:left;width:16%"
+        class="select-input"
+        v-model="value"
+        filterable
+        placeholder="类型"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-option>
+      </el-select>
+      <el-input
+        size="mini"
+        style="width:30%;float:left;margin-left:6%"
+        placeholder="请输入商品名称"
+        v-model="input"
+        clearable
+      ></el-input>
+      <el-button type="primary" size="mini" style="margin-left:-20%" plain>查询</el-button>
+      <div>
+        <el-table :data="table1" max-height="400" border-top="1px solid black">
+          <el-table-column fixed="left" prop="name" label="商品名称" width="150"></el-table-column>
+          <el-table-column prop="type" label="类型" width="120"></el-table-column>
+          <el-table-column fixed="right" label="操作" width="120">
+            <template slot-scope="scope">
+              <el-button @click.native.prevent="add(scope.row)" type="text" size="small">添加</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div style="position:fixed;right:10%;top:15%">
+        <el-card 
+      shadow="never"
+        >
+        <div slot="header" class="clearfix">
+    <span style="float:left;color:blue">库存</span>
+  </div>
+          <el-table :data="table1" max-height="395">
+            <el-table-column fixed="left" prop="name" label="商品名称" width="150"></el-table-column>
+            <el-table-column prop="type" label="类型" width="120"></el-table-column>
+            <el-table-column prop="nums" label="数量" width="100"></el-table-column>
+            <el-table-column fixed="right" label="操作" width="120">
+              <template slot-scope="scope">
+                <el-button @click.native.prevent="delete(scope.row)" type="text" size="small">添加</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-card>
+      </div>
+    </el-card>
+    <div>
+      <el-dialog :visible.sync="dialogVisible" width="30%">
+        <el-input-number
+          style="float:left;margin-top:2%;margin-left:5%;"
+          v-model="nums"
+          :min="1"
+          :max="10"
+          label="描述文字"
+        ></el-input-number>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="deleteAction">确 定</el-button>
+        </span>
+      </el-dialog>
+    </div>
   </div>
 </template>
 
 <script>
 import { merchandiseManagement } from "./merchandise-management";
 export default {
-mixins:[merchandiseManagement]
-}
+  mixins: [merchandiseManagement]
+};
 </script>
 
 <style>
-
+.el-input.is-active .el-input__inner,
+.el-input__inner:focus {
+  border-color: #22a7f0 !important;
+}
 </style>
