98bb7a3ee651a7d293bd4503091345d98baaa886
merlynr <lcq1013962426@gmail.com>
2020-03-27 18:17:13 +0800

    前后购物，订单完成

--- src/views/shop-order/shop-order.js
+++ src/views/shop-order/shop-order.js
@@ -1,7 +1,37 @@
+import { Message } from "element-ui";
 export const shopOrder={
     data(){
         return{
-            
+            orderList:[]
         }
+    },
+    methods:{
+        getOrder(){
+            this.GET('/api/cart/search?userId='+Number(this.storage.getItem('userId')) + "&" + "status=finish").then(res=>{
+                this.orderList=res.result.data
+            })
+        },
+        removeCart(index,id){
+            let param={
+                id:id
+            }
+            this.POST('/api/cart/remove',param).then(res => {
+                Message({
+                    message: res.result.message,
+                    duration: 2000,
+                    type: "success"
+                })
+                this.orderList.splice(index,1)
+            }).catch(e=>{
+                Message({
+                    message: e,
+                    duration: 2000,
+                    type: "error"
+                })
+            })
+        }
+    },
+    mounted(){
+        this.getOrder()
     }
 }
--- src/views/shop-order/shop-order.vue
+++ src/views/shop-order/shop-order.vue
@@ -12,12 +12,57 @@
         <span style="color:red">重要提醒：</span>近期假冒客服诈骗事件频发，有货不会以任何理由向您索取账户密码或引导转账。请您提高警惕，谨防诈骗。
       </div>
       <div>
-        <div>
-
+        <div style="width:100%;background:#efefef;height:40px;text-align:left">
+          <span style="display:inline-block;width:40%;margin-left:20px;height:40px;line-height:40px"> 
+            <span style="display:inline-block;width:20px"></span>商品信息</span>
+          <span style="display:inline-block;width:10%;height:40px;line-height:40px;text-align:center"> 单价（元）</span>
+          <span style="display:inline-block;width:10%;height:40px;line-height:40px;text-align:center"> 数量</span>
+          <span style="display:inline-block;width:15%;height:40px;line-height:40px;text-align:center"> 实付款（元）</span>
+          <span style="display:inline-block;width:10%;height:40px;line-height:40px;text-align:center"> 订单状态</span>
+          <span style="display:inline-block;width:10%;height:40px;line-height:40px;text-align:center"> 操作</span>
+        </div>
+        <div style="margin-top:10px" v-for="(item,key) of orderList" :key="key">
+          <div class="order" style="width:95%;border:0.5px solid #e6e6e6;margin:10px auto;height:115px">
+            <div style="width:100%;height:28px;background:#efefef;line-height:28px;text-align:left;font-size:10px">
+              <span style="display:inline-block;width:10px"></span>订单编号：{{item.Order}}<span style="display:inline-block;width:20px"></span>下单时间：{{item.Time}}
+            </div>
+            <el-image
+              style="width:64px;height:64px;margin-left:10px;margin-top:10px;display:inline-block;float:left"
+              :src="item.Shops.Goods.Img"
+            />
+            <div style="height:85px;width:170px;margin-left:95px;margin-top:17px;text-align:left">
+              {{item.Shops.Goods.Name}}
+              <div style="width:80%;height:20px;color:#9a9a9a;position:relative;top:10px">
+                颜色: 黑色
+                <span style="display:inline-block;width:10px" /> 尺码：L
+              </div>
+            </div>
+            <div
+              style="width:145px;height:85px;line-height:85px;margin-left:260px;margin-top:-105px"
+            >￥{{item.Shops.Goods.Money}}</div>
+            <div style="margin-top:-45px;margin-left:124px;">{{item.Num}}</div>
+            <el-divider direction="vertical" style="height:18em"></el-divider>
+            <div style="margin-top:-55px;margin-left:300px">
+            {{item.Num*item.Shops.Goods.Money }}
+            <div style="color:#afb6bf">(免快递)</div>
+            </div>
+            <div style="margin-top:-25px;margin-left:500px;color:#afb6bf">
+            交易成功
+            </div>
+            <div class="buxiangxiele">
+            <el-divider direction="vertical"></el-divider>
+            </div>
+            <div class="kuaitule">
+            <el-divider direction="vertical"></el-divider>
+            </div>
+            <div style="margin-top:-70px;color:red;margin-left:650px">
+              <el-button size="mini" type="text" @click="removeCart(key,item.Id)">移除</el-button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </div>  
 </template>
 
 <script>
@@ -28,4 +73,19 @@ export default {
 </script>
 
 <style>
+.order .el-divider--vertical{
+height:7.1em !important;
+margin-top: -51px;
+margin-left:130px
+}
+.buxiangxiele .el-divider--vertical{
+height:7.1em !important;
+margin-top: -51px;
+margin-left:330px
+}
+.kuaitule .el-divider--vertical{
+height:7.1em !important;
+margin-top: -101px;
+margin-left:580px
+}
 </style>
